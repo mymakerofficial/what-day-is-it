df5188324b72d4a4e39802adbc0a88446512e9bf
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2021-03-15 23:28:45 +0100

    First working version

--- src/App.vue
+++ src/App.vue
@@ -15,13 +15,6 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "@/sass/app.scss";
 </style>
--- src/components/DayContainer.vue
+++ src/components/DayContainer.vue
@@ -1,9 +1,10 @@
 <template>
-  <div v-html="dayHTML"></div>
+  <div class="dayContainer"  v-html="dayHTML"></div>
 </template>
 
 <script>
 
+import axios from 'axios'
 const marked = require("marked");
 
 export default {
@@ -11,15 +12,37 @@ export default {
 
   data() {
     return {
-      dayText: "# Its *some day*",
+      days: [],
+      dayText: "# *Monday*\n > mano fuente mas democracia fútbol cerebro",
     }
   },
 
   computed: {
+    currentDayTexts: function () {
+      return this.days[new Date().getDay()]
+    },
     dayHTML: function () {
       return marked(this.dayText)
     },
   },
+
+  methods: {
+    start(){
+      this.dayText = this.currentDayTexts[Math.random()*this.currentDayTexts.length]
+    },
+    loadData(){
+      axios.get(`/data/days.json`).then(response => {
+        this.days = response.data.days
+        this.start()
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+  },
+
+  created() {
+    this.loadData()
+  }
 }
 </script>
 
