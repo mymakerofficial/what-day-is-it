050383360db5658560af7769ca9e75c9f6f57238
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2021-03-15 20:56:40 +0100

    initial commit

---
+++ src/components/DayContainer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-html="dayHTML"></div>
+</template>
+
+<script>
+
+const marked = require("marked");
+
+export default {
+  name: "DayContainer",
+
+  data() {
+    return {
+      dayText: "# Its *some day*",
+    }
+  },
+
+  computed: {
+    dayHTML: function () {
+      return marked(this.dayText)
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
