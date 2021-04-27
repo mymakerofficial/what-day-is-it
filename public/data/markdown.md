ca7603d26103e2ca0f8dcf874165c4b91bd4e331
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2021-04-27 18:00:58 +0200

    added markdown guide (not really)

---
+++ public/data/markdown.md
@@ -0,0 +1,8 @@
+### Basic Syntax
+
+| Element | Markdown Syntax |
+| --- | --- |
+| [Heading](https://www.markdownguide.org/basic-syntax/#headings) | `# heading` |
+| [Bold](https://www.markdownguide.org/basic-syntax/#bold) | `**bold text**` |
+| [Italic](https://www.markdownguide.org/basic-syntax/#italic) | `*italicized text*` |
+| [Blockquote](https://www.markdownguide.org/basic-syntax/#blockquotes-1) | `> blockquote` |
---
+++ src/views/MarkdownGuide.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <Header :headerTitle="headerTitle" :title="title" :style="{ backgroundColor: this.currentDay.color.hsl, color: this.currentDay.color.hslInverted }"></Header>
+    <Body :text="text" :navButtons="navButtons"></Body>
+    <Footer :navButtons="navButtons"></Footer>
+  </div>
+</template>
+
+<script>
+import Footer from "../components/Footer";
+import Body from "../components/Body";
+import Header from "../components/Header";
+import {Day} from "../js/day";
+import axios from "axios";
+
+export default {
+  name: "MarkdownGuide",
+  components: {Footer, Body, Header},
+
+  data() {
+    return {
+      headerTitle: "Markdown Guide",
+      title: 'Markdown makes your text look good',
+      text: '',
+      data: {"days": [],"any":[]},
+      date: new Date(),
+      currentDay: new Day(),
+    }
+  },
+
+  computed: {
+    navButtons: function () {
+      return [
+        {text: "today", path: "/", display: true},
+        {text: "day forecast", path: "/forecast", display: true},
+        {text: "custom day", path: "/custom", display: true},
+        {text: "about", path: "/about", display: true}
+      ]
+    }
+  },
+
+  methods: {
+    start: function () {
+      //get start of day
+      if(process.env.VUE_APP_ALWAYS_USE_START_OF_DAY === "true" && !this.setDay){
+        let date = new Date()
+        this.year = date.getFullYear()
+        this.month = date.getMonth()+1
+        this.day = date.getDate()
+      }
+
+      //get date
+      if(this.year !== undefined && this.month !== undefined && this.day !== undefined) this.date = new Date(this.year, this.month-1, this.day, 0, 0, 0, 0);
+
+
+      //start day
+      this.currentDay.set(this.date, this.data)
+    },
+    loadData(){
+      // load day text database
+      axios.get(`/data/markdown.md`).then(response => {
+        this.text = response.data
+        axios.get(`/data/days.json`).then(response => {
+          this.data = response.data
+          this.start()
+        }).catch(error => {
+          console.log(error)
+        })
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+  },
+
+  created() {
+    this.loadData()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
