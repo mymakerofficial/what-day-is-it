686d7f2eaca36e3300b016bb32c673d998627497
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2021-05-12 14:55:21 +0200

    removed a console log

--- src/views/ThingOfTheDay.vue
+++ src/views/ThingOfTheDay.vue
@@ -45,7 +45,6 @@ export default {
       ]
     },
     footerText: function () {
-      console.log(this.message)
       return this.message.author ? `this <b>${this.thing.fullName}</b> was brought to you by <b>${this.message.author}</b><br>` : "" + this.thing.source && this.thing.sourceText ? `source: <b><a href="${this.thing.source}">${this.thing.sourceText}</a></b>` : ""
     },
     title: function () {
