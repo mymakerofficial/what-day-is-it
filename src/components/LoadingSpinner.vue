3ca15b5077503001ee64d7bb2ee842f7fad63124
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2021-05-22 18:16:43 +0200

    fixed small error in LoadingSpinner

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -34,20 +34,22 @@ export default {
   methods: {
     setup: function () {
       this.$nextTick(function () {
-        this.animation = anime({
-          targets: this.$refs.container.children,
-          rotate: [0, 0, () => {
-            return anime.random(-6, 6)
-          }, 0],
-          translateY: [0, 8, -10, 0],
-          scaleX: [1, 1.2, 0.9, 1],
-          scaleY: [1, 0.8, 1.1, 1],
-          duration: 1400,
-          delay: anime.stagger(100, {start: -100}),
-          easing: 'spring(1, 80, 10, 0)',
-          autostart: true,
-          loop: true,
-        })
+        if(this.$refs.container){
+          this.animation = anime({
+            targets: this.$refs.container.children,
+            rotate: [0, 0, () => {
+              return anime.random(-6, 6)
+            }, 0],
+            translateY: [0, 8, -10, 0],
+            scaleX: [1, 1.2, 0.9, 1],
+            scaleY: [1, 0.8, 1.1, 1],
+            duration: 1400,
+            delay: anime.stagger(100, {start: -100}),
+            easing: 'spring(1, 80, 10, 0)',
+            autostart: true,
+            loop: true,
+          })
+        }
       })
     }
   },
